5d343a573386379f61ad214a1e13b171c80b60c3
GrapeDay <6020290@mborijnland.nl>
2023-10-31 21:57:55 +0100

    Starting logs

--- resources/views/logs.blade.php
+++ resources/views/logs.blade.php
@@ -9,7 +9,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <div class="flex justify-between">
+                        <p class="font-bold">Title</p>
+                        <p class="font-bold">Text</p>
+                        <p class="font-bold">Deadline</p>
+                        <p class="font-bold">Updated at</p>
+                        <p class="font-bold">Actions</p>
+                    </div>
                 </div>
             </div>
         </div>
