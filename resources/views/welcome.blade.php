ed76877a3f2818a0c7eb51fbfd39e829ef71aea0
GrapeDay <6020290@mborijnland.nl>
2023-11-01 14:48:07 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,28 +4,37 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Sign in</title>
+        <title>homepage</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/brands.css">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/fontawesome.css">
-        <link rel="stylesheet" href="https://cdn.web.bas.ac.uk/bas-style-kit/0.5.0/css/bas-style-kit.min.css">
+        <link href="https://fonts.cdnfonts.com/css/akira-expanded" rel="stylesheet">
+        <style>.txt{font-family: 'Akira Expanded', sans-serif;}</style>
         <script src="https://cdn.tailwindcss.com"></script>
     </head>
-    <body class="antialiased">
-        <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-white bg-center bg-gray-100 selection:bg-red-500 selection:text-white">
-            {{-- @if (Route::has('login'))
-                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                    @auth
-                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
-                    @else
-                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
-                    @endauth
+    <body class="antialiased bg-gray-200">
+
+        <!-- login knop -->
+        <a href="{{ route('azure.login') }}" class="flex h-screen items-center justify-center ">
+            <div class="image">
+                <img height="75px" width="75px" src="https://cdn.discordapp.com/attachments/1148578717828907049/1169263696640487454/key.png?ex=6554c4ad&is=65424fad&hm=6e694aac0c53acd9a159b9886a156a95cddbf8132e53d36fa935f2aa1c0f0fb3&" alt="">
+            </div>
+            <div class="streep"></div>
+            <div class="text">
+                <div class="login txt text-5xl">login</div>
+                <div class="flex items-center justify-center">
+                    <img height="25px" width="25px" src="https://cdn.discordapp.com/attachments/1148578717828907049/1169263695654821928/Azure.png?ex=6554c4ac&is=65424fac&hm=9536f9127afdcdcd7e50747d21ba1e8a3bc464f0a17ed2c07d63cc332f306dc9&" alt="">
+                    <div class="azure txt text-sm">with azure</div>
                 </div>
-            @endif --}}
-            <a href="{{ route('azure.login') }}" class="bsk-btn bsk-btn-default"><i class="fab fa-microsoft"></i> Sign in with Azure</a>
-        </div>
+            </div>
+        </a>
+        <!-- login knop eind -->
+
+        <!-- hierzo moet homepage komen -->
+        <!-- sidebar -->
+        <!-- searchbar -->
+        <!-- hier moeten de logs komen zet die neer met dummy text -->
+
     </body>
 </html>
