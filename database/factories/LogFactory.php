a1964d6d6aa73f39893966f77f27c7f97ff19925
YagoSchipper <6022749@mborijnland.nl>
2023-11-01 14:34:58 +0100

    date dummy data update

--- database/factories/LogFactory.php
+++ database/factories/LogFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\Log;
 
 /**
@@ -21,7 +22,7 @@ class LogFactory extends Factory
 
         return [
             'level' => $this->faker->numberBetween(1,4),
-            'date' => $this->faker->dateTime('d/m/Y H:i:s'),
+            'date' => $this->faker->dateTimeBetween('d/m/Y H:i:s','-3 hour'),
             'source' => 'WindowsUpdateClient',
             'eventID' => $this->faker->numberBetween(1,50),
             'Category' => 'Windows Update Client'
